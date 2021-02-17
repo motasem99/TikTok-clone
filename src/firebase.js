c05187ef2ddd38a982806704934b4105ed43a08f
mutasem <motasemimad10@gmail.com>
2021-02-17 12:36:18 +0200

    finish footer

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyArqgjI4oSr4S_OE7zO8znC5wA1i-P22UQ',
+  authDomain: 'tiktok-clone-a0463.firebaseapp.com',
+  projectId: 'tiktok-clone-a0463',
+  storageBucket: 'tiktok-clone-a0463.appspot.com',
+  messagingSenderId: '279839154716',
+  appId: '1:279839154716:web:7bba09932b01452536b5d9',
+  measurementId: 'G-QD9V3DW7TY',
+};
